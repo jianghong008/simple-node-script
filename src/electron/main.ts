0c8cd6c5429c8e9065c444ec91cc7024a38f2d20
jianghong <jianghong2020@qq.com>
2024-06-04 13:13:26 +0800

    git 移出out

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,12 +1,17 @@
 import { app, BrowserWindow } from 'electron'
 import path from 'path'
+import { protocol } from 'electron'
+protocol.registerSchemesAsPrivileged([
+  { scheme: 'foo', privileges: { bypassCSP: true } }
+])
 function createWindow() {
     const win = new BrowserWindow({
         width: 800,
         height: 600,
         webPreferences: {
             nodeIntegration: true,
-            preload:path.join(process.cwd(), 'out/preload.js')
+            preload:path.join(process.cwd(), 'out/preload.js'),
+            
         }
     })
     
@@ -17,6 +22,7 @@ function createWindow() {
     }else{
         win.loadFile('dist/index.html')
     }
+    
     
 }
 
